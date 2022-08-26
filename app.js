1b91ecffde1a59434ccd397a421048059fa860dd
dariuszKlimko <dariuszklimkoportfolio@gmail.com>
2022-08-27 01:33:58 +0200

    'third'

--- app.js
+++ app.js
@@ -4,14 +4,14 @@ const app = express();
 const PORT = 8000;
 
 // there is no asynchronous function inside both endpoints so I don't use await/async or then
-app.get('/calculateDieselUsageForDistance', (req, res) => {
-    const {distance, yearOfProduction, fuelUsagePer100KM} = req.query;
+app.get('/calculateDieselUsageForDistance/:distance/:yearOfProduction/:fuelUsagePer100KM', (req, res) => {
+    const {distance, yearOfProduction, fuelUsagePer100KM} = req.params;
     const fuelUsage = distance*fuelUsagePer100KM/100;
     return res.send(`${fuelUsage}`);
 });
 
-app.get('/probabilityOfUnitInjectorFail', (req, res) => {
-    const {VIN} = req.query;
+app.get('/probabilityOfUnitInjectorFail/:VIN', (req, res) => {
+    const {VIN} = req.params;
     const failProbability = Math.floor(Math.random()*101);
     return res.send(`${failProbability}%`);
 });
